dc959db2c7574c7f471cc81e94b35e1d365737ce
sclandinin <sclandinin@gmail.com>
2016-12-08 22:40:36 -0600

    Add files via upload

---
+++ Pylon_voice_recognition.ino
@@ -0,0 +1,42 @@
+
+const int pylon =  2;      // the number of the LED pin
+byte response = 0;     //reply from voice recognition
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(pylon, OUTPUT);
+  
+  delay(1000);
+  Serial.write(0xAA);
+  Serial.write(0x37);
+  delay(1000);
+  Serial.write(0xAA);
+  Serial.write(0x21);
+  delay(1000);
+}
+
+void loop() 
+{
+  while(Serial.available())
+  {
+    response = Serial.read();
+
+    //"You must construct additional pylons."
+    if (response == 0x11) 
+    {
+      delay(750);
+      digitalWrite(pylon, HIGH);
+    }
+
+    //"My life for Aiur!"
+    if (response == 0x12) 
+    {
+      delay(500);
+      digitalWrite(pylon, LOW);
+    }
+    
+  }
+}
+
+
